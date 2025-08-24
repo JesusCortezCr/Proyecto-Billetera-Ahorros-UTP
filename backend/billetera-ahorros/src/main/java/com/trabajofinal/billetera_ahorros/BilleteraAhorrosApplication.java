83bbbed686ed897d811ef38d09df6bd51e6a9b35
Cortez Jesus <cortez_laloramos@hotmail.com>
2025-08-24 12:50:30 -0500

    Se agrego estructura principal frontend y backend

---
+++ backend/billetera-ahorros/src/main/java/com/trabajofinal/billetera_ahorros/BilleteraAhorrosApplication.java
@@ -0,0 +1,13 @@
+package com.trabajofinal.billetera_ahorros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilleteraAhorrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilleteraAhorrosApplication.class, args);
+	}
+
+}
---
+++ backend/billetera-ahorros/src/test/java/com/trabajofinal/billetera_ahorros/BilleteraAhorrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabajofinal.billetera_ahorros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilleteraAhorrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
